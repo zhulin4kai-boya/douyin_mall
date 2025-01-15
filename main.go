68806d5065e3b075c56a91b92550b5c65815f453
zhulin4kai <2720460362@qq.com>
2025-01-15 13:26:34 +0800

    basic frame

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+)
+
+func main() {
+	// server.Default() creates a Hertz with recovery middleware.
+	// If you need a pure hertz, you can use server.New()
+	h := server.Default()
+
+	h.GET("/hello", func(ctx context.Context, c *app.RequestContext) {
+		c.String(consts.StatusOK, "Hello hertz!")
+	})
+
+	h.Spin()
+}
